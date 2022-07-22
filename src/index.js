76d2ac7709c6699700129a2769f76303f5cb71fe
Vinicius Benso <vinibenso@gmail.com>
2022-07-22 15:48:33 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const {v4: uuidv4} = require('uuid');
+const app = express();
+
+app.use(express.json());
+
+
+const customers =[];
+
+app.post("/account", (req, res) => {
+  const {cpf, name} = req.body;
+
+  const customersAlreadyExist = customers.some((customer)=> customer.cpf === cpf);
+
+  if(customersAlreadyExist) {
+    return res.status(400).json({error: 'Customer already exists'})
+  };
+
+  const id = uuidv4();
+  customers.push(
+    {
+      cpf,
+      name,
+      id: uuidv4(),
+      statement: []
+    });
+
+    return res.status(201).send();
+})
+
+
+
+
+
+app.listen(3000);
